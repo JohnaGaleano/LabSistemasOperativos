c70419b91ff24fe3e0b060b4b216ae9b4101ec6c
johnagaleano <john.galeanoc@gmail.com>
2018-10-27 17:53:42 -0500

    modified

--- Taller 5 - minibash/Teoria/EjerciciosTaller3/ejercicio6Mod.c
+++ Taller 5 - minibash/Teoria/EjerciciosTaller3/ejercicio6Mod.c
@@ -1,31 +1,32 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <wait.h>
 
 int main(int argc, char *argv[])
 {
     pid_t pid_h1;
     pid_t pid_n;
-    int i = 0;
+    int status_h1, status_n;
     pid_h1 = fork();
     if (pid_h1 == 0)
     {
-        i++;
         pid_n = fork();
         if (pid_n == 0)
         {
-            i++;
             printf("!\n");
         }
         else
-        {
+        {            
             printf("Mundo ");
+
         }
     }
 
     else
     {
-        printf("Hola");
+        printf("Hola ");
+
     }
 
-return 0;
+    return 0;
 }
